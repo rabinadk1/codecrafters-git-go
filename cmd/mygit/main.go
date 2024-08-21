a80c8ae08be03d17af6137c98750d014a0731f4d
Rabin Adhikari <rabin.adk1@gmail.com>
2024-08-21 16:30:25 +0545

    pass 3rd stage

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
@@ -11,55 +15,108 @@ import (
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
-		os.Exit(1)
+		log.Fatalln("usage: mygit <command> [<args>...]")
 	}
 
 	switch command := os.Args[1]; command {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+				log.Fatalf("Error creating directory: %s\n", err)
 			}
 		}
 
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+			log.Fatalf("Error writing file: %s\n", err)
 		}
 
 		fmt.Println("Initialized git directory")
 
 	case "cat-file":
 		if len(os.Args) != 4 {
-			fmt.Fprintf(os.Stderr, "usage: mygit cat-file <type> <object>\n")
-			os.Exit(1)
+			log.Fatalln("usage: mygit cat-file <type> <object>")
 		}
 
 		sha := os.Args[3]
 		path := fmt.Sprintf(".git/objects/%v/%v", sha[:2], sha[2:])
 		compressedReader, err := os.Open(path)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
-			os.Exit(1)
+			log.Fatalf("Error reading file: %s\n", err)
 		}
 		defer compressedReader.Close()
 
 		z, err := zlib.NewReader(compressedReader)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
-			os.Exit(1)
+			log.Fatalf("Error decompressing file: %s\n", err)
 		}
 
 		defer z.Close()
 
 		p, err := io.ReadAll(z)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading decompressed reader: %s\n", err)
-			os.Exit(1)
+			log.Fatalf("Error reading decompressed reader: %s\n", err)
 		}
+
 		parts := strings.Split(string(p), "\x00")
 		fmt.Print(parts[1])
+
+	case "hash-object":
+
+		if len(os.Args) < 3 {
+			log.Fatalln("usage: mygit hash-object [-w] <file>")
+		}
+
+		thirdArg := os.Args[2]
+
+		writeObject := thirdArg == "-w"
+
+		var filepath string
+		if writeObject {
+			if len(os.Args) < 4 {
+				log.Fatalln("usage: mygit hash-object -w <file>")
+			}
+
+			filepath = os.Args[3]
+		} else {
+			filepath = thirdArg
+		}
+
+		content, err := os.ReadFile(filepath)
+		if err != nil {
+			log.Fatalf("Error opening file: %s\n", err)
+		}
+
+		size := len(content)
+
+		prefix := []byte(fmt.Sprintf("blob %v\x00", size))
+
+		content = append(prefix, content...)
+
+		h := sha1.New()
+		if _, err := h.Write(content); err != nil {
+			log.Fatalf("Error hashing file: %s\n", err)
+		}
+
+		hash := hex.EncodeToString(h.Sum(nil))
+
+		fmt.Println(hash)
+
+		if writeObject {
+			var compressedBytes bytes.Buffer
+			w := zlib.NewWriter(&compressedBytes)
+			w.Write(content)
+			w.Close()
+
+			writeDir := ".git/objects/" + hash[:2]
+			if err := os.Mkdir(writeDir, 0755); err != nil {
+				log.Fatalf("Error creating directory: %s\n", err)
+			}
+
+			if err := os.WriteFile(writeDir+"/"+hash[2:], compressedBytes.Bytes(), 0644); err != nil {
+				log.Fatalf("Error writing file: %s\n", err)
+			}
+		}
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
