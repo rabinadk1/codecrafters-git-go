3b3ccada51755922213f08ffdc89cf252768d635
Rabin Adhikari <rabin.adk1@gmail.com>
2024-08-21 15:49:08 +0545

    pass 2nd stage

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
@@ -26,6 +29,37 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+
+	case "cat-file":
+		if len(os.Args) != 4 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file <type> <object>\n")
+			os.Exit(1)
+		}
+
+		sha := os.Args[3]
+		path := fmt.Sprintf(".git/objects/%v/%v", sha[:2], sha[2:])
+		compressedReader, err := os.Open(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+			os.Exit(1)
+		}
+		defer compressedReader.Close()
+
+		z, err := zlib.NewReader(compressedReader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
+			os.Exit(1)
+		}
+
+		defer z.Close()
+
+		p, err := io.ReadAll(z)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading decompressed reader: %s\n", err)
+			os.Exit(1)
+		}
+		parts := strings.Split(string(p), "\x00")
+		fmt.Print(parts[1])
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
